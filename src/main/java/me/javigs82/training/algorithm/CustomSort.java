7fe97fadb6f97af216895027786fa572eab59a31
javigs82 <javigs82@gmail.com>
2019-03-25 13:08:27 +0100

    replace for statement by IntStream

--- src/main/java/me/javigs82/training/algorithm/CustomSort.java
+++ src/main/java/me/javigs82/training/algorithm/CustomSort.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.stream.IntStream;
 
 import static java.util.Map.Entry.comparingByValue;
 
@@ -16,20 +17,23 @@ public class CustomSort {
 
         arr.stream().forEach(value -> freq.merge(value, 1, Integer::sum));
 
-        freq.entrySet().stream().sorted(comparingByValue()).forEach(
-                e -> {
-                    for (int i = 0; i < e.getValue(); i++) {
-                        System.out.println(e.getKey());
-                    }
-                }
-        );
-
+        freq.entrySet()
+                .stream()
+                .sorted(comparingByValue())
+                .forEach( e ->  IntStream.range(0, e.getValue())
+                        .forEach( i -> System.out.println(e.getKey())));
     }
 
     public static final void main(String[] arg) {
         List<Integer> input = new ArrayList<>();
         input.add(1);
         input.add(2);
+        input.add(2);
+        input.add(2);
+        input.add(2);
+        input.add(3);
+        input.add(3);
+        input.add(3);
         customSort(input);
     }
 
